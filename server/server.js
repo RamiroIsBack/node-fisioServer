a559d2f472dc18b185dd2e7115eb480a882f1b3e
ra <ra@ras-MacBook-Air.local>
2018-10-10 15:02:09 -0400

    express server up and running

--- server/server.js
+++ server/server.js
@@ -1,3 +1,19 @@
-const path = require('path');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const publicPath= path.join(__dirname , '/public');
+const publicPath = path.join(__dirname, "../public");
+
+var app = express();
+const port = process.env.PORT || 3000;
+//app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   res.send(publicPath);
+// });
+app.use(express.static(publicPath));
+app.listen(port, () => {
+  console.log(`starting on port: ${port}`);
+});
+
+module.exports = { app };
