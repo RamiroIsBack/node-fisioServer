78e0be7ab2396b97cc80b099ffca29d047416a34
ra <ramiro.santamaria.alvarez@gmail.com>
2018-10-27 10:10:19 -0400

    starting point of integrating graphQl with Apollo-server into this express server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const { generateMessage } = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 
@@ -10,11 +10,11 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 const port = process.env.PORT || 3000;
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.get("/", (req, res) => {
-//   res.send(publicPath);
-// });
+app.get("/users/login", (req, res) => {
+  res.send(publicPath);
+});
 app.use(express.static(publicPath));
 io.on("connection", socket => {
   console.log("new User conected");
