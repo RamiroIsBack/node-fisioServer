34fb538d404aba0e6ec57d0ef1b000f528dc073f
ra <ramiro.santamaria.alvarez@gmail.com>
2018-10-29 14:50:52 -0400

    setting port to process.env.PORT ||4000 to enable heroku server to work

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -4,11 +4,12 @@ const { ApolloServer } = require("apollo-server-express");
 const { TYPEDEFS } = require("./types.js");
 const { RESOLVERS } = require("./resolvers.js");
 // GraphQL: Schema
+const port = process.env.PORT || 4000;
 const SERVER = new ApolloServer({
   typeDefs: TYPEDEFS,
   resolvers: RESOLVERS,
   playground: {
-    endpoint: `http://localhost:4000/graphql`,
+    endpoint: `http://localhost:${port}/graphql`,
     settings: {
       "editor.theme": "light"
     }
